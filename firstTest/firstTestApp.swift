3b59b396db3e06af03b9ec15507270e345061a1e
oki2207 <115452565+oki2207@users.noreply.github.com>
2023-02-13 10:19:54 +0100

    Initial Commit

---
+++ firstTest/firstTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstTestApp.swift
+//  firstTest
+//
+//  Created by Olli on 13.02.23.
+//
+
+import SwiftUI
+
+@main
+struct firstTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
