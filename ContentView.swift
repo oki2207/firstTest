4b0ada10af91707f31ebc4c547c2f98feb1fc882
oki2207 <115452565+oki2207@users.noreply.github.com>
2023-02-13 12:39:39 +0100

    test xcode

--- ContentView.swift
+++ ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, you first app!")
+            Text("Hello, your first app!")
         }
         .padding()
     }
